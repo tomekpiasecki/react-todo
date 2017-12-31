9b51d7a5ee9adfce763ca32ef828a5aff7a04d7e
Tomek Piasecki <netpiaseq+github@gmail.com>
2017-12-31 11:28:35 +0100

    todos rendered inline

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -2,6 +2,24 @@ import React from 'react'
 import ToDosListHeader from './todos-list-header'
 
 class ToDosList extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: [
+                {
+                    task: 'Walk the dog',
+                    isCompleted: true
+                },
+                {
+                    task: 'clean the desk',
+                    isCompleted: false
+                }
+            ]
+        }
+    }
+    getTaskClasses(todo) {
+        return `task ${todo.isCompleted ? 'completed' : ''}`
+    }
     render() {
         return (
             <table>
@@ -9,6 +27,12 @@ class ToDosList extends React.Component {
                     columns = {['Tasks', 'Actions']}
                 />
                 <tbody>
+                    {this.state.todos.map((todo, index) => 
+                        <tr key={'todo_' + index}>
+                            <td className={this.getTaskClasses(todo)}>{todo.task}</td>
+                            <td className="actions">{todo.task} - actions</td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         )
